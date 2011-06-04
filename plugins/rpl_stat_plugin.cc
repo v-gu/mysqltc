27ace15d24f584d57afb4ecae5f68176e2e8b0fd
Vincent Gu <LiSNAz@gmail.com>
2011-06-04 23:33:16 +0800

    rc1

--- plugins/rpl_stat_plugin.cc
+++ plugins/rpl_stat_plugin.cc
@@ -2,17 +2,18 @@
    Replication plugin for MySQL.
 */
 
-//#include <stdlib.h>
+#include <stdlib.h>
 #include <mysql/plugin.h>
 #include "sql_class.h"                          // THD
+#include <replication.h>
 #include <hash.h>
 
-int is_rpl_stat_before_send_event(Binlog_transmit_param *param,
-                                unsigned char *packet, unsigned long len,
-                                const char *log_file, my_off_t log_pos)
+int rpl_stat_before_send_event(Binlog_transmit_param *param,
+                               unsigned char *packet, unsigned long len,
+                               const char *log_file, my_off_t log_pos)
 {
   uint32 server_id = param->server_id;
-  sql_print_error=("server_id: %d, log_file: %s", server_id, log_file);
+  sql_print_error("server_id: %d, log_file: %s", server_id, log_file);
   return 0;
 }
 
@@ -26,20 +27,20 @@ Binlog_transmit_observer transmit_observer = {
   NULL,                             // start
   NULL,                             // stop
   NULL,                             // reserve_header
-  is_rpl_stat_before_send_event,    // before_send_event
+  rpl_stat_before_send_event,       // before_send_event
   NULL,                             // after_send_event
   NULL,                             // reset
 };
 
-static int is_rpl_stat_plugin_init(void *p)
+static int rpl_stat_plugin_init(void *p)
 {
   if (register_binlog_transmit_observer(&transmit_observer, p))
     return 1;
-  sql_print_error=("*p's name: %s",(*(st_plugin_int) p)->name);
+  sql_print_error("*p's name: %s", ((st_plugin_int *)p)->name.str);
   return 0;
 }
 
-static int is_rpl_stat_plugin_deinit(void *p)
+static int rpl_stat_plugin_deinit(void *p)
 {
   return 0;
 }
@@ -49,17 +50,17 @@ static int is_rpl_stat_plugin_deinit(void *p)
    build plugin requisites
 */
 struct Mysql_replication rpl_stat_plugin
-{ MYSQL_INFORMATION_SCHEMA_INTERFACE_VERSION };
+{ MYSQL_REPLICATION_INTERFACE_VERSION };
 
 struct st
 
 /**
   plugin library descriptor
 */
-mysql_declare_plugin(repl_stat_plugin)
+mysql_declare_plugin(rpl_stat)
 {
   MYSQL_REPLICATION_PLUGIN,
-  &rpl_stat,
+  &rpl_stat_plugin,
   "replication_progress_plugin",
   "Vincent Gu (lisnaz@gmail.com)",
   "A plugin which will send replication progress info to I_S table on master",
