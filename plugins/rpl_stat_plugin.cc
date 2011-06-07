13613e747597bbcc622ded4e604ff309d96affec
Vincent Gu <LiSNAz@gmail.com>
2011-06-07 15:58:21 +0800

    test1

--- plugins/rpl_stat_plugin.cc
+++ plugins/rpl_stat_plugin.cc
@@ -2,19 +2,37 @@
    Replication plugin for MySQL.
 */
 
-#include <stdlib.h>
+//#include <stdlib.h>
+#include <my_global.h>
 #include <mysql/plugin.h>
 #include "sql_class.h"                          // THD
-#include <replication.h>
 #include <hash.h>
+#include <replication.h>
+
+#define LOG_LINE_BUFFER_SIZE 80
+
+File log_f;
+
+struct rpl_stat_context
+{
+  unsigned long events_count;
+  File rpl_stat_log_file;
+};
 
 int rpl_stat_before_send_event(Binlog_transmit_param *param,
                                unsigned char *packet, unsigned long len,
                                const char *log_file, my_off_t log_pos)
 {
-  uint32 server_id = param->server_id;
-  sql_print_error("server_id: %d, log_file: %s", server_id, log_file);
-  return 0;
+  uint32 server_id= param->server_id;
+  char line_buf[LOG_LINE_BUFFER_SIZE];
+  
+  my_snprintf(line_buf, sizeof(line_buf),
+              "server_id:%d, binlog_file:%s, offset:%d\n",
+              server_id, log_file, log_pos);
+  extern File log_f;
+  my_write(log_f, (uchar*) line_buf, strlen(line_buf), MYF(0));
+  
+  DBUG_RETURN(0);
 }
 
 
@@ -27,22 +45,61 @@ Binlog_transmit_observer transmit_observer = {
   NULL,                             // start
   NULL,                             // stop
   NULL,                             // reserve_header
-  rpl_stat_before_send_event,       // before_send_event
+  rpl_stat_before_send_event,    // before_send_event
   NULL,                             // after_send_event
   NULL,                             // reset
 };
 
 static int rpl_stat_plugin_init(void *p)
 {
+  DBUG_ENTER("rpl_stat_plugin_init");
+  struct rpl_stat_context *context;
+  char log_filename[FN_REFLEN];
+
+  struct st_plugin_int *plugin= (struct st_plugin_int *)p;
+
+  context= (struct rpl_stat_context *)
+    my_malloc(sizeof(struct rpl_stat_context), MYF(0));
+  context->events_count= 0;
+  fn_format(log_filename, "rpl-stat", "", ".log",
+            MY_REPLACE_EXT | MY_UNPACK_FILENAME);
+  unlink(log_filename);
+  context->rpl_stat_log_file= my_open(log_filename, O_CREAT|O_RDWR, MYF(0));
+  extern File log_f;
+  log_f= context->rpl_stat_log_file;
+
   if (register_binlog_transmit_observer(&transmit_observer, p))
+  {
+    // registration failed
+    my_close(context->rpl_stat_log_file, MYF(0));
+    my_free(context);
     return 1;
-  sql_print_error("*p's name: %s", ((st_plugin_int *)p)->name.str);
-  return 0;
+  } else{
+    plugin->data= (void *)context;
+  }
+
+  sql_print_information("*plugin %s regisitered", plugin->name.str);
+  DBUG_RETURN(0);
 }
 
 static int rpl_stat_plugin_deinit(void *p)
 {
-  return 0;
+  DBUG_ENTER("rpl_stat_plugin_deinit");
+
+  struct st_plugin_int *plugin= (struct st_plugin_int *)p;
+  struct rpl_stat_context *context= (struct rpl_stat_context *)plugin->data;
+
+  my_close(context->rpl_stat_log_file, MYF(0));
+
+  my_free(context);
+
+  if (unregister_binlog_transmit_observer(&transmit_observer, p))
+  {
+    sql_print_error("unregister_binlog_transmit_observer failed");
+    DBUG_RETURN(1);
+  }
+  
+  DBUG_RETURN(0);
 }
 
 
@@ -52,18 +109,17 @@ static int rpl_stat_plugin_deinit(void *p)
 struct Mysql_replication rpl_stat_plugin
 { MYSQL_REPLICATION_INTERFACE_VERSION };
 
-struct st
 
 /**
   plugin library descriptor
 */
-mysql_declare_plugin(rpl_stat)
+mysql_declare_plugin(rpl_stat_plugin)
 {
   MYSQL_REPLICATION_PLUGIN,
   &rpl_stat_plugin,
-  "replication_progress_plugin",
+  "rpl_stat_plugin",
   "Vincent Gu (lisnaz@gmail.com)",
-  "A plugin which will send replication progress info to I_S table on master",
+  "A plugin which will write replication progress info to log file on master",
   PLUGIN_LICENSE_GPL,
   rpl_stat_plugin_init,
   rpl_stat_plugin_deinit,
